78c7065185e8d681a6eaba599cdb9b0cffc97b65
Meysam Noori <hshmatullahnoor@gmail.com>
2025-10-29 02:09:19 -0700

    Update API and images URLs to production endpoints

--- src/api/main.ts
+++ src/api/main.ts
@@ -1,4 +1,4 @@
 
-const API_URL = "http://localhost:8000/api";
-export const IMAGES_URL = "http://localhost:8000/storage/";
+const API_URL = "https://api2.skyscript.site/api";
+export const IMAGES_URL = "https://api2.skyscript.site/storage/";
 export default API_URL;
